9880b39590e4c1294cb94b8386f896268971a86a
Lily <ms.lilypeng@gmail.com>
2021-02-20 23:14:13 -0500

    Stable build featuring search-as-you-type and scroll-to-top

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,13 +1,14 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Searchbar.css';
 import MovieCard from './MovieCard.js';
 
 function Searchbar() {
     const [movies, setMovies] = useState([]);
     const [query, setQuery] = useState('');
-    const [showMore, setShowMore] = useState(true);
+    const [showMore, setShowMore] = useState(false);
     const [totalResults, setTotalResults] = useState(-1);
     const pageRef = useRef(1);
+    const [showScroll, setShowScroll] = useState(false);
 
 
     const fetchApi = async (resetResults) => {
@@ -17,19 +18,34 @@ function Searchbar() {
             const response = await fetch(apiUrl);
             const data = await response.json();
             resetResults ? setMovies(data.results) : setMovies(movies.concat(data.results));
-            pageRef.current === data.total_pages ? setShowMore(true) : setShowMore(false);
+            pageRef.current === data.total_pages ? setShowMore(false) : setShowMore(true);
             setTotalResults(data.total_results);
         } catch (error) {
-            console.error(error + " No More Movies to Display");
+            console.error(`Sorry, the TMDb database fetch request has failed: ${error}`);
         }
     }
 
-    const querySearch = e => {
-        e.preventDefault();
-        if (!query) return;
-        pageRef.current = 1;
-        fetchApi(true);
-    }
+    const checkScrollTop = () => {
+        if (!showScroll && window.pageYOffset > 400){
+            setShowScroll(true)
+        } else if (showScroll && window.pageYOffset <= 400){
+            setShowScroll(false)
+        }
+    };
+
+    window.addEventListener('scroll', checkScrollTop);
+
+    useEffect(() => {
+        if (query.length > 2) {
+            if (!query) return;
+            pageRef.current = 1;
+            fetchApi(true);
+        } else {
+            setMovies([]);
+            setTotalResults(-1);
+            setShowMore(false);
+        }
+    }, [query]);
 
     const loadMore = e => {
         e.preventDefault();
@@ -37,36 +53,44 @@ function Searchbar() {
         fetchApi(false);
     }
 
+    const scrollTop = () =>{
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+
     return (
         <>
-            <form className="search-form" onSubmit={querySearch}>
-                <label className="movie-label" htmlFor="search">Movie:</label>
+            <form className="search-form" onSubmit={e => e.preventDefault()}>
+                <label className="movie-label" htmlFor="search">Movie Title:</label>
                 <input 
                     className="searchbar"
                     name="search"
                     type="text" 
-                    placeholder="e.g. Parasite" 
+                    placeholder="e.g. Jurassic Park, Parasite, etc..." 
                     value={query}
                     onChange={e => setQuery(e.target.value)}
                 />
-                <button className="btn-search">Search</button>
+                
             </form>
             <div>
                 {totalResults < 0 ? null 
-                    : totalResults === 1 ? <h2>{totalResults} movie found</h2>
-                    : <h2>{totalResults} movies found</h2>}
+                    : totalResults === 1 ? <h2 className="results-num">{totalResults} movie found</h2>
+                    : <h2 className="results-num">{totalResults} movies found</h2>}
                 <div className="cards-container">
                     <div className="movie-cards">
                         {movies.map(movie =>
                                 <MovieCard movie={movie} key={movie.id} />
                             )}
                     </div>
-                    {showMore ? null : <button className="btn-show-more" onClick={loadMore} type="button">Show More</button>}
+                    {showMore ? <button className="btn-show-more" onClick={loadMore} type="button">Show More</button> : null}
                 </div>
             </div>
-            
-            
-            
+            <button 
+                className={showScroll ? "btn-scroll" : "btn-scroll fade-out"} 
+                onClick={scrollTop} 
+                type="button">
+                    <p className="arrow">↑</p>
+                    <p>Back to Top</p>
+            </button>
         </>
     )
 }
@@ -74,19 +98,17 @@ function Searchbar() {
 export default Searchbar
 
 /* TODO: 
-
-    - css grid for large screens
-    - hide results initially
-    - hide "load more" initially
-
+    
+    - auto search (search-as-you-type)
 
     - scroll bar to read description?
     - scroll to top button
-    - auto search?
     - sort by popularity
     - sort by release date
     - filter by genre
     - switch for TV shows
     - better css styling
     - better error handling
+
+                    style={{display: showScroll ? 'flex' : 'none'}} 
 */
